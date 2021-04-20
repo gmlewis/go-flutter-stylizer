2f1877183cf95bf80d98c35c72e5cc86ddefe1da
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-04-20 16:43:57 -0400

    Implement findLineIndexAtOffset

--- dart/editor.go
+++ dart/editor.go
@@ -16,6 +16,38 @@ limitations under the License.
 
 package dart
 
-func (c *Class) findMatchingBracket(openParenOffset int) int {
+import "log"
+
+// findMatchingBracket finds the matching closing "}" (for "{") or ")" (for "(")
+// given the offset of the opening rune.
+func (c *Class) findMatchingBracket(openOffset int) int {
+	open := c.fullBuf[openOffset : openOffset+1]
+	if open != "{" && open != "(" {
+		log.Fatalf("ERROR: findMatchingBracket(%v) called on non-bracket %q. Please file a bug report on the GitHub issue tracker.", openOffset, open)
+	}
+	searchFor := "}"
+	if open == "(" {
+		searchFor = ")"
+	}
+
+	return c.findClosing(openOffset, searchFor)
+}
+
+// findClosing finds searchFor and returns its absolute offset.
+func (c *Class) findClosing(openOffset int, searchFor string) int {
+	lineIndex, relOffset := c.findLineIndexAtOffset(openOffset)
+	log.Printf("lineIndex=%v, relOffset=%v", lineIndex, relOffset)
 	return 0
+}
+
+// findLineIndexAtOffset finds the line index and relative offset for the
+// absolute offset within the text buffer.
+func (c *Class) findLineIndexAtOffset(openOffset int) (int, int) {
+	for i, line := range c.lines {
+		if len(line.line) > openOffset {
+			return i, openOffset
+		}
+		openOffset -= len(line.line) + 1
+	}
+	return len(c.lines), openOffset
 }
--- dart/editor_test.go
+++ dart/editor_test.go
@@ -9,6 +9,24 @@ import (
 //go:embed basic_classes.dart.txt
 var basicClasses string
 
+func setupClass(t *testing.T, searchFor, buf string) (*Class, int, int) {
+	t.Helper()
+	classOffset := strings.Index(buf, searchFor)
+	openCurlyOffset := classOffset + len(searchFor) - 1
+	closeCurlyOffset := len(buf) - 2
+
+	if got, want := buf[classOffset:openCurlyOffset+1], searchFor; got != want {
+		t.Errorf("offset error: buf[%v:%v] = %q, want %q", classOffset, openCurlyOffset+1, got, want)
+	}
+	if got, want := buf[closeCurlyOffset:closeCurlyOffset+1], "}"; got != want {
+		t.Errorf("offset error: buf[%v:%v] = %q, want %q", closeCurlyOffset, closeCurlyOffset+1, got, want)
+	}
+
+	c := NewClass("Class1", classOffset, openCurlyOffset, closeCurlyOffset, false)
+	c.FindFeatures(buf)
+	return c, openCurlyOffset, closeCurlyOffset
+}
+
 func TestFindMatchingBracket(t *testing.T) {
 	bc, bcOCO, bcCCO := setupClass(t, "class Class1 {", basicClasses)
 
@@ -84,20 +102,92 @@ func TestFindMatchingBracket(t *testing.T) {
 	}
 }
 
-func setupClass(t *testing.T, searchFor, buf string) (*Class, int, int) {
-	t.Helper()
-	classOffset := strings.Index(buf, searchFor)
-	openCurlyOffset := classOffset + len(searchFor) - 1
-	closeCurlyOffset := len(buf) - 2
+func TestFindLineIndexAtOffset(t *testing.T) {
+	bc, bcOCO, _ := setupClass(t, "class Class1 {", basicClasses)
 
-	if got, want := buf[classOffset:openCurlyOffset+1], searchFor; got != want {
-		t.Errorf("offset error: buf[%v:%v] = %q, want %q", classOffset, openCurlyOffset+1, got, want)
-	}
-	if got, want := buf[closeCurlyOffset:closeCurlyOffset+1], "}"; got != want {
-		t.Errorf("offset error: buf[%v:%v] = %q, want %q", closeCurlyOffset, closeCurlyOffset+1, got, want)
+	tests := map[string]struct {
+		class         *Class
+		openOffset    int
+		wantLineIndex int
+		wantRelOffset int
+	}{
+		"top-level": {
+			class:         bc,
+			openOffset:    bcOCO,
+			wantLineIndex: 6,
+			wantRelOffset: 13,
+		},
+		"build()": {
+			class:         bc,
+			openOffset:    strings.Index(basicClasses, "build()") + 5,
+			wantLineIndex: 10,
+			wantRelOffset: 7,
+		},
+		"build() {}": {
+			class:         bc,
+			openOffset:    strings.Index(basicClasses, "build() {}") + 8,
+			wantLineIndex: 10,
+			wantRelOffset: 10,
+		},
+		"Class1();": {
+			class:         bc,
+			openOffset:    strings.Index(basicClasses, "Class1();") + 6,
+			wantLineIndex: 29,
+			wantRelOffset: 8,
+		},
+		"Class1.fromNum();": {
+			class:         bc,
+			openOffset:    strings.Index(basicClasses, "Class1.fromNum();") + 14,
+			wantLineIndex: 30,
+			wantRelOffset: 16,
+		},
+		"var myfunc = (int n) => n;": {
+			class:         bc,
+			openOffset:    strings.Index(basicClasses, "var myfunc = (int n) => n;") + 13,
+			wantLineIndex: 31,
+			wantRelOffset: 15,
+		},
+		"toString()": {
+			class:         bc,
+			openOffset:    strings.Index(basicClasses, "toString()") + 8,
+			wantLineIndex: 34,
+			wantRelOffset: 10,
+		},
+		"toString() {": {
+			class:         bc,
+			openOffset:    strings.Index(basicClasses, "toString() {") + 11,
+			wantLineIndex: 34,
+			wantRelOffset: 13,
+		},
+		"print('$_pvi, $_spv, $_spvni, $_pvini, ${sqrt(2)}');": {
+			class:         bc,
+			openOffset:    strings.Index(basicClasses, "print('$_pvi, $_spv, $_spvni, $_pvini, ${sqrt(2)}');") + 5,
+			wantLineIndex: 35,
+			wantRelOffset: 9,
+		},
+		"${sqrt(2)}": {
+			class:         bc,
+			openOffset:    strings.Index(basicClasses, "${sqrt(2)}") + 1,
+			wantLineIndex: 35,
+			wantRelOffset: 44,
+		},
+		"sqrt(2)": {
+			class:         bc,
+			openOffset:    strings.Index(basicClasses, "sqrt(2)") + 4,
+			wantLineIndex: 35,
+			wantRelOffset: 49,
+		},
 	}
 
-	c := NewClass("Class1", classOffset, openCurlyOffset, closeCurlyOffset, false)
-	c.FindFeatures(buf)
-	return c, openCurlyOffset, closeCurlyOffset
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			gotLineIndex, gotRelOffset := tt.class.findLineIndexAtOffset(tt.openOffset)
+			if gotLineIndex != tt.wantLineIndex {
+				t.Errorf("findMatchingBracket(%v) lineIndex = %v, want %v", tt.openOffset, gotLineIndex, tt.wantLineIndex)
+			}
+			if gotRelOffset != tt.wantRelOffset {
+				t.Errorf("findMatchingBracket(%v) relOffset = %v, want %v", tt.openOffset, gotRelOffset, tt.wantRelOffset)
+			}
+		})
+	}
 }
