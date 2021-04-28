47722156db130a7025afad2a6353d4af92f21f8f
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-04-28 11:38:44 -0400

    Remove unused comments

--- dart/editor.go
+++ dart/editor.go
@@ -99,50 +99,6 @@ func (e *Editor) findStartOfClass(startOffset int) int {
 	return 0
 }
 
-// // findMatchingBracket finds the matching closing "}" (for "{") or ")" (for "(")
-// // given the offset of the opening rune.
-// func (e *Editor) findMatchingBracket(openOffset int) (int, error) {
-// 	open := e.fullBuf[openOffset : openOffset+1]
-// 	if open != "{" && open != "(" {
-// 		return 0, fmt.Errorf("findMatchingBracket(%v) called on non-bracket %q. Please file a bug report on the GitHub issue tracker", openOffset, open)
-// 	}
-// 	searchFor := "}"
-// 	if open == "(" {
-// 		searchFor = ")"
-// 	}
-
-// 	absOffset := openOffset + 1
-// 	lineIndex, relStrippedOffset := e.findLineIndexAtOffset(absOffset)
-// 	if lineIndex >= len(e.lines) {
-// 		return 0, fmt.Errorf("findLineIndexAtOffset(%v) returned lineIndex(=%v) past last line %v", absOffset, lineIndex, len(e.lines))
-// 	}
-
-// 	e.logf("lineIndex=%v, relStrippedOffset=%v, stripped=%q(%v), line=%q(%v)", lineIndex, relStrippedOffset, e.lines[lineIndex].stripped, len(e.lines[lineIndex].stripped), e.lines[lineIndex].line, len(e.lines[lineIndex].line))
-
-// 	reader := strings.NewReader("")
-// 	if relStrippedOffset < 0 {
-// 		return 0, fmt.Errorf("findLineIndexAtOffset(%v) returned bad relStrippedOffset(=%v) for stripped line %q(%v)", absOffset, relStrippedOffset, e.lines[lineIndex].stripped, len(e.lines[lineIndex].stripped))
-// 	}
-// 	if relStrippedOffset < len(e.lines[lineIndex].stripped) {
-// 		reader = strings.NewReader(e.lines[lineIndex].stripped[relStrippedOffset:])
-// 	}
-
-// 	cursor := &Cursor{
-// 		e:         e,
-// 		absOffset: absOffset,
-// 		lineIndex: lineIndex,
-
-// 		relStrippedOffset: relStrippedOffset,
-
-// 		reader: reader,
-// 	}
-// 	if _, err := cursor.advanceUntil(searchFor); err != nil {
-// 		return 0, fmt.Errorf("advanceUntil(%q): %v", searchFor, err)
-// 	}
-
-// 	return cursor.absOffset - 1 - len(cursor.runeBuf), nil
-// }
-
 // findLineIndexAtOffset finds the line index and relative offset for the
 // absolute offset within the text buffer. Note that the returned
 // relStrippedOffset may be greater than the length of the stripped string
