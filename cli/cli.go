243d277cb39943cd2fe37d259ef0289fc1393a57
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-12-12 16:40:30 -0500

    0.1.5

--- cli/cli.go
+++ cli/cli.go
@@ -31,7 +31,7 @@ import (
 )
 
 // This version string probably can not reasonably stay in sync with the VSCode plugin, unfortunately.
-const VERSION = "0.1.4"
+const VERSION = "0.1.5"
 
 var cfgFile string
 
